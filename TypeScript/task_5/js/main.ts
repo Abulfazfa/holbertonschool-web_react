324abc62a427d38a8a7b2e312755836703d7814c
Abulfazfa <abulfazfa@code.edu.az>
2024-11-08 21:26:13 +0400

    TypeScript_finish

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,24 @@
+interface MajorCredits {
+	credits: number;
+	brand: 'brandA';
+}
+
+interface MinorCredits {
+	credits: number;
+	brand: 'brandB';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+      credits: subject1.credits + subject2.credits,
+      brand: "brandA",
+    };
+  }
+  
+  function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+      credits: subject1.credits + subject2.credits,
+      brand: "brandB",
+    };
+  }
+  
